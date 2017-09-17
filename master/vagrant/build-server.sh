9a064e7568e6fcbd64aed4e306c86f408a384d35
jsantanajr <cadastro_josewilson@outlook.com>
2017-09-17 18:03:04 -0300

    Create build-server.sh

---
+++ master/vagrant/build-server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ -e /etc/redhat-release ]]; then
+
+# Percona's Yum Repository
+yum -d 0 -y install http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm epel-release
+# Install Packages
+yum -d 0 -y install Percona-Server-server-56 Percona-Server-shared-56 Percona-Server-client-56 Percona-Server-shared-compat percona-toolkit percona-xtrabackup vim-enhanced pigz pwgen
+
+# Setup mysql and disable iptables
+
+/sbin/chkconfig mysql on
+/sbin/chkconfig iptables off
+
+/sbin/service iptables stop
+
+# Copy a my.cnf slave to server
+
+rm -f /etc/my.cnf
+cp /new_slave_automatic/vagrant/master.cnf /etc/my.cnf
+
+/etc/init.d/mysql start
+
+fi
